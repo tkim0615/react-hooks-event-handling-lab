66353768bbff5d7851315e1519535847fa6aa008
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-09 21:36:47 -0500

    completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    return (
+        <div>   
+            <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </div>
+
+    )
+}
+
+export default EyesOnMe
+
+// Eyes on the ball
+// Let's say you're in the club with your buddy. 
+// The music's blaring, lights are flashing... It's so hard to get his attention! 
+// Your job is to create a component that registers whenever he focuses on you, and when his eyes are drifting off.
+
+// In the components/EyesOnMe.js file, create a EyesOnMe React component.
+// In that component, render a button with the text 'Eyes on me'.
+// On that button, add event handlers that listen for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,25 @@
 // Code Keypad Component Here
+import React from "react"
+
+
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <form>
+                <input onChange={()=>console.log('Entering password...')} type='password'></input>
+            </form>
+        </div>
     )
 }
 
 export default Keypad;
+
+// Mr. Burns has requested us to build a new keypad component for the nuclear plant,
+// since the last one was way too complicated for his employees to use. 
+// We'll keep things super simple instead, and use an <input type="password" /> field to capture input. Here's how to complete the exercise:
+
+// In the components/Keypad.js file, create a Keypad React component.
+// In that component, render an input with the right type.
+// On that input, add an event handler that listens for the change event.
+// When that event fires, use console.log to print out the text 'Entering password...'.
